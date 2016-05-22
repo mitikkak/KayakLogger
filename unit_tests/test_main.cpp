33f6ea54b1cb078270f5c73f0ad00e7a35608bc3
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2016-05-23 00:13:23 +0300

    starting using unit test framework

---
+++ unit_tests/test_main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Testing KayakLogger" << endl;
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
+
