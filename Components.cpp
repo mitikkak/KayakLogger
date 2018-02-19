f3ad7545c7cf6dbf03eff10e2ab827df88216867
Mikko <mikko.tikkakoski@gmail.com>
2018-02-19 20:19:01 +0200

    adding esp8266 spi and sd sources

--- Components.cpp
+++ Components.cpp
@@ -21,4 +21,4 @@ LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 unsigned long prevTimeTiltHandled = 0;
 unsigned long prevTimeGpsHandled = 0;
 AverageSpeed averageSpeed;
-Distance distance;
+Distance distance_;
