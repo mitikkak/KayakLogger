ff8a9c53720b4b4c9b364154d815547fec9601db
Mikko Tikkakoski <mikko.tikkakoski@nokia.com>
2018-07-13 14:42:00 +0300

    google test as submodule

--- unit_tests/stubs/Arduino.h
+++ unit_tests/stubs/Arduino.h
@@ -3,9 +3,8 @@
 
 #include <string.h>
 void pinMode(const int pin, const int mode);
-namespace Arduino
-{
-    extern long timeNow;
+namespace Arduino {
+extern long timeNow;
 }
 long millis();
 void digitalWrite(const int pin, const int state);
@@ -15,7 +14,7 @@ static const int OUTPUT = 1;
 static const int HIGH = 1;
 static const int LOW = 0;
 typedef char byte;
-typedef unsigned int size_t;
+// typedef unsigned int size_t;
 
 static const int A0 = 90;
 static const int A1 = 91;
@@ -24,4 +23,4 @@ static const int A3 = 93;
 
 static constexpr int DEC{3};
 
-char * 	itoa (int val, char *s, int radix);
+char *itoa(int val, char *s, int radix);
